68b975cf8136c939da2112789bfea23e852614cf
song__ <35736920+SimDaSong@users.noreply.github.com>
2019-02-09 22:33:57 +0900

    그대로 출력하기 2

---
+++ 11719.cpp
@@ -0,0 +1,20 @@
+/*
+	https://www.acmicpc.net/problem/11719
+	그대로 출력하기2
+	
+    11718번과 소스 동일
+*/
+
+#include <iostream>
+
+int main() {
+	char ch = 0;
+
+	while (true) {
+		ch = getchar();
+		if (ch == EOF) return 0;
+		putchar(ch);
+	}
+
+	return 0;
+}
