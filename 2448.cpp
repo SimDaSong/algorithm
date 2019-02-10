9adb7f59bd9180378a0c2d70b572d48199afa9da
SimDaSong <song98987@korea.ac.kr>
2019-02-10 12:53:09 +0900

    별찍기 - 11

---
+++ 2448.cpp
@@ -0,0 +1,16 @@
+/*
+	https://www.acmicpc.net/problem/2448
+	º°Âï±â - 11
+*/
+
+#include <iostream>
+
+void printThreeStar(){
+	for (int i = 0; i < 3; i++) {
+		for (int j=0;j<)
+		printf("")
+	}
+}
+int main() {
+
+}
